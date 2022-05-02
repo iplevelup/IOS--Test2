f5fc50011afc35c4f165570ed9389caa08fcbb1f
Igor Podvysotsky <jcosmic@yandex.ru>
2022-05-02 11:48:24 +0300

    First Initial

---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by sv on 12.04.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+        var closure: (() -> Void)?
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            self.view.backgroundColor = .white
+        }
+    }
